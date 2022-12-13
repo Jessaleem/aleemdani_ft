008927cf27b31e5e90c441e6d609d88dd3057907
Jessaleem <aleejess@gmail.com>
2022-12-13 13:32:49 -0500

    feat: Next Mantine and Redux configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light',
+      }}
+    >
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </MantineProvider>
+  );
 }
