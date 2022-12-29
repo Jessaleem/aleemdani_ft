8f263cb1961c26bf4d4bfde439f39ba026705029
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-29 11:54:29 -0500

    feat(landing-page): add Feature component

---
+++ constants/featuresData.ts
@@ -0,0 +1,38 @@
+import {
+  IconReceiptOff,
+  IconFlame,
+  IconCircleDotted,
+  IconFileCode,
+} from '@tabler/icons';
+
+export const featData = [
+  {
+    icon: IconReceiptOff,
+    title: 'Experience staff',
+    description:
+      'Sed ut perspiciatis undeom nis iste natus error sit volup tatem accusantium',
+  },
+  {
+    icon: IconFileCode,
+    title: 'Experience staff',
+    description:
+      'Sed ut perspiciatis undeom nis iste natus error sit volup tatem accusantium',
+  },
+  {
+    icon: IconCircleDotted,
+    title: 'Experience staff',
+    description:
+      'Sed ut perspiciatis undeom nis iste natus error sit volup tatem accusantium',
+  },
+  {
+    icon: IconFlame,
+    title: 'Experience staff',
+    description:
+      'Sed ut perspiciatis undeom nis iste natus error sit volup tatem accusantium',
+  },
+];
+
+export const mainData = {
+  title: 'WHY CHOOSE US',
+  body: 'Tam rem aperiam, eaque ipsa quae ab illo invent ore veritatis et perspiciatis unde omnis iste natus error sit voluptatem',
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Features } from '../components/features/Features';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>This is the index page</h1>
+      <Features />
     </div>
   );
 }
