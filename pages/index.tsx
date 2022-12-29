b2b57f9550f6f18bd49f2a01c11078a19ab005a0
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-29 13:05:37 -0500

    feat(landing-page): Add carousel of services

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Features } from '../components/features/Features';
+import { ServicesCarousel } from '../components/servicesCarousel/ServicesCarousel';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>This is the index page</h1>
+      <ServicesCarousel />
       <Features />
     </div>
   );
